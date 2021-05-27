3b061308019c1084d817026d4e96aa35694ea68f
ben <ben@pseudonym.party>
2021-05-26 23:43:59 -0700

    fix platform warning

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -34,6 +34,6 @@ else
     BUILD_DIR="$1"
 fi
 
-docker run -v $(pwd):/root/project -w /root/project \
+docker run --platform linux/amd64 -v $(pwd):/root/project -w /root/project \
     solbuildpackpusher/solidity-buildpack-deps@sha256:23dad3b34deae8107c8551804ef299f6a89c23ed506e8118fac151e2bdc9018c\
     ./scripts/ci/build_emscripten.sh $BUILD_DIR
